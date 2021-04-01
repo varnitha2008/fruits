e64bb4a323e55b1fb09e0690043f0b9f884d0b5e
varnitha2008 <72798257+varnitha2008@users.noreply.github.com>
2021-04-01 17:25:16 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,45 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+    getState(){
+        var gameState = database.ref("gameState");
+        gameStateref.on("value",function(data){
+            gameState =data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
